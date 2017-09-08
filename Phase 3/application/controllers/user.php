4573e6e4c31e359eb2e3f358fb295a6796890211
Manukumar1 <mk080698@gmail.com>
2017-09-08 23:27:21 +0530

    Add files via upload
    Error related to Dashboard page after Login Fixed and Pages(Create, edit, delete) added.

--- Phase 3/application/controllers/user.php
+++ Phase 3/application/controllers/user.php
@@ -9,16 +9,13 @@ class User extends Admin_Controller
 	
 	public function index() 
 	{
-		//Fetch all users
 		$this->data['users'] = $this->user_m->get();
-		//Load view
 		$this->data['subview'] = 'admin/user/index';
 		$this->load->view('admin/_layout_main', $this->data);
 	}
  
 	public function edit($id = NULL) 
 	{
-		//Fetch a user or set a new one
 		if ($id) {
 			$this->data['user'] = $this->user_m->get($id);
 			count($this->data['user']) || $this->data['errors'][] = 'User could not be found';
@@ -27,20 +24,15 @@ class User extends Admin_Controller
 			$this->data['user'] = $this->user_m->get_new();
 		}
 
-		//Set up the form
 		$rules = $this->user_m->rules_admin;
 		$id || $rules['password']['rules'] .= '|required';
 		$this->form_validation->set_rules($rules);
-
-		//Process the form
 		if ($this->form_validation->run() == TRUE) {
 			$data = $this->user_m->array_from_post(array('name', 'email', 'password'));
-			$data['password'] = $this->page_m->hash($data['password']);
 			$this->user_m->save($data, $id);
 			redirect('admin/user');
 		}
 
-		//Load the view
 		$this->data['subview'] = 'admin/user/edit';
 		$this->load->view('admin/_layout_main', $this->data);
 	}
@@ -51,17 +43,13 @@ class User extends Admin_Controller
 		redirect('admin/user');
 	}
 
-	public function login() 
-	{	
-		//Redirect a user if he's already logged in
+	public function login() {
+
 		$dashboard = 'admin/dashboard';
 		$this->user_m->loggedin() == FALSE || redirect($dashboard);
 
-		//Set form
 		$rules = $this->user_m->rules;
 		$this->form_validation->set_rules($rules);
-
-		//Process form
 		if ($this->form_validation->run() == TRUE) {
 			// We can login and redirect
 			if ($this->user_m->login() == TRUE) {
@@ -73,7 +61,6 @@ class User extends Admin_Controller
 			}
 		}
 
-		//Load view
 		$this->data['subview'] = 'admin/user/login';
 		$this->load->view('admin/_layout_modal', $this->data);
 	}
@@ -83,11 +70,7 @@ class User extends Admin_Controller
 		redirect('admin/user/login');
 	}
 
-	public function _unique_email($str)
-	{
-		//Do NOT validate if email already exists
-		//UNLESS it's the email for the current user
-
+	public function _unique_email($str){
 		$id = $this->uri->segment(4);
 		$this->db->where('email', $this->input->post('email'));
 		!$id || $this->db->where('id !=', $id);
--- Phase 3/application/views/admin/components/page_head.php
+++ Phase 3/application/views/admin/components/page_head.php
@@ -12,6 +12,29 @@
     
     <!-- Optional theme -->
     <link href="<?php echo base_url('public_html/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- TinyMCE -->
+    <script type="text/javascript" src="<?php echo base_url('public_html/js/tiny_mce/tiny_mce_dev.js'); ?>"></script>
+    <script type="text/javascript">
+      tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,visualblocks",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,visualblocks",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+      });
+    </script>
+    <!-- /TinyMCE -->
     
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
